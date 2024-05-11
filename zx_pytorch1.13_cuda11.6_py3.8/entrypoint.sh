faf403eb9755fed13801709f33ceb8f61341345e
Dreamupers <zhangxi19982017@gmail.com>
2024-05-11 18:25:14 +0800

    bug

--- zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
+++ zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
@@ -5,4 +5,5 @@ find /home/dockeru -group 1000 -exec chgrp -h dockeru {} \;
 cd /home/dockeru
 echo "{\"editor.fontSize\": 18,\"workbench.colorTheme\": \"GitHub Light\"}" >> /home/dockeru/.local/share/code-server/User/settings.json
 chown dockeru:dockeru /home/dockeru/.local/share/code-server/User/settings.json
+chown dockeru:dockeru /home/dockeru/requirements.txt
 exec gosu dockeru code-server --host 0.0.0.0 --locale=zh-cn
