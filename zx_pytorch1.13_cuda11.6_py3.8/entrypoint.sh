dc21c72dad96c1aa7f14d834fb3794e3220d0cbd
Dreamupers <zhangxi19982017@gmail.com>
2024-05-11 12:11:50 +0800

    codeserver

--- zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
+++ zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
@@ -1,6 +1,7 @@
 echo 'export PATH=/home/dockeru/.local/bin:$PATH' >> /home/dockeru/.zshrc
 usermod -u $UID dockeru 
 groupmod -g $GID dockeru
-chown -R dockeru:dockeru /home/dockeru
+# chown -R dockeru:dockeru /home/dockeru
+find /home/dockeru -group 1000 -exec chgrp -h dockeru {} \;
 cd /home/dockeru
-exec gosu dockeru $@
+exec gosu dockeru code-server --host 0.0.0.0
