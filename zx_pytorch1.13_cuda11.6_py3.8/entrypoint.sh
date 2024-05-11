84b2c12d3030c363033ca7939bf344f3887964fd
Dreamupers <zhangxi19982017@gmail.com>
2024-05-11 16:37:53 +0800

    code

--- zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
+++ zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
@@ -4,4 +4,6 @@ groupmod -g $GID dockeru
 # chown -R dockeru:dockeru /home/dockeru
 find /home/dockeru -group 1000 -exec chgrp -h dockeru {} \;
 cd /home/dockeru
+echo "{\"editor.fontSize\": 18,\"workbench.colorTheme\": \"GitHub Light\"}" >> /home/dockeru/.local/share/code-server/User/settings.json
+echo "{\"locale\": \"zh-cn\"}" >> /home/dockeru/.local/share/code-server/User/argv.json
 exec gosu dockeru code-server --host 0.0.0.0
