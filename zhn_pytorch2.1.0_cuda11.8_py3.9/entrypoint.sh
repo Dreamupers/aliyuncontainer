f7258cc89ab981f5f1905a789203d22c7450b5cb
Dreamupers <zhangxi19982017@gmail.com>
2024-05-11 20:21:28 +0800

    zhn

--- zhn_pytorch2.1.0_cuda11.8_py3.9/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-echo 'export PATH=/home/dockeru/.local/bin:$PATH' >> /home/dockeru/.zshrc
-usermod -u $UID dockeru 
-groupmod -g $GID dockeru
-chown -R dockeru:dockeru /home/dockeru
-cd /home/dockeru
-exec gosu dockeru code-server --host 0.0.0.0
