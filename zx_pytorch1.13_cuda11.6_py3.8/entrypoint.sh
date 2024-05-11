fd670be274de0e6f74ad3addb80f2700b590ba16
Dreamupers <zhangxi19982017@gmail.com>
2024-05-11 20:17:39 +0800

    fixuid

--- zx_pytorch1.13_cuda11.6_py3.8/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-echo 'export PATH=/home/dockeru/.local/bin:$PATH' >> /home/dockeru/.zshrc
-usermod -u $UID dockeru 
-groupmod -g $GID dockeru
-find /home/dockeru -group 1000 -exec chgrp -h dockeru {} \;
-cd /home/dockeru
-echo "{\"editor.fontSize\": 18,\"workbench.colorTheme\": \"GitHub Light\"}" >> /home/dockeru/.local/share/code-server/User/settings.json
-chown dockeru:dockeru /home/dockeru/.local/share/code-server/User/settings.json
-exec gosu dockeru code-server --host 0.0.0.0 --locale=zh-cn
